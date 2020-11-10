1fa6a267ec7f73a7fe891c2eaf7861e368117a82
meed223 <henryrlewis@btinternet.com>
2020-11-10 16:07:20 +0000

    Added game-type exception for failing to update type.

---
+++ src/main/java/com/meed223/rms_editor/exceptions/UpdateGameTypeException.java
@@ -0,0 +1,11 @@
+package com.meed223.rms_editor.exceptions;
+
+public class UpdateGameTypeException extends Exception {
+	public UpdateGameTypeException() {
+		
+	}
+	
+	public UpdateGameTypeException(String errorMessage) {
+		super(errorMessage);
+	}
+}
